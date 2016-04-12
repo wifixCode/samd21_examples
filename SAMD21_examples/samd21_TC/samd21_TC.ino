f1c334a2c95ddd0f53398cf567145e4d826773d8
Ricardo Mena C <rgacort12@gmail.com>
2016-04-12 14:28:20 -0600

    Timer counter examples renamed

---
+++ SAMD21_examples/samd21_TC/samd21_TC.ino
@@ -0,0 +1,3 @@
+Unstaged changes after reset:
+M	SAMD21_examples/samd21_TC/samd21_TC.ino
+D	SAMD21_examples/samd21_TC_config/samd21_TC_config.ino
---
+++ SAMD21_examples/samd21_TCC/samd21_TCC.ino
@@ -0,0 +1,5 @@
+Unstaged changes after reset:
+M	SAMD21_examples/samd21_TC/samd21_TC.ino
+M	SAMD21_examples/samd21_TCC/samd21_TCC.ino
+D	SAMD21_examples/samd21_TCC_config/samd21_TCC_config.ino
+D	SAMD21_examples/samd21_TC_config/samd21_TC_config.ino
